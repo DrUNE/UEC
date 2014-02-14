792bf162cf7e6677597729741dac18650d1001c1
Andrey Litvinov <andreilitvinov@yahoo.co.uk>
2014-02-14 12:43:02 +0400

    The Feedback form now posting data  by using RESTful API

--- src/main/frontend/app/scripts/controllers/main.js
+++ src/main/frontend/app/scripts/controllers/main.js
@@ -2,34 +2,52 @@
 
 angular.module('feedbackControllers', [])
 
-    .controller('FeedbackCtrl', function($scope, $location){
-        angular.extend($scope, {
-            feedback : {
-                message:'',
-                name:'',
-                email:'',
-                captcha: null
+    .controller('FeedbackCtrl', function($scope, $location, $http){
+        var
+            postFeedback = function(){
+                $http
+                    .post('/rest/feedbacks', feedbackData($scope.feedback))
+                    .success(onPostSuccess);
             },
 
-            submitPressed: false,
-
-            isValid: function(formInput){
-                return formInput.$invalid && $scope.submitPressed;
+            onPostSuccess = function(data){
+                $location.path('/feedbackSendSuccess');
             },
 
-            sendFeedback: function(form){
-                $scope.submitPressed = true;
-                if(form.$valid){
-                    $location.path('/feedbackSendSuccess')
-                }
-            },
+            feedbackData = function(feedback) {
+                return {
+                    "id":null,
+                    "message":feedback.message,
+                    "fullName":feedback.name,
+                    "email":feedback.email
+                };
+            };
 
-            captchaImgSrcTime: new Date().getTime(),
+        $scope.feedback = {
+            message:'',
+            name:'',
+            email:'',
+            captcha: null
+        };
 
-            refreshCaptchaImg: function(){
-                $scope.captchaImgSrcTime = new Date().getTime();
+        $scope.submitPressed = false;
+
+        $scope.isValid = function(formInput){
+            return formInput.$invalid && $scope.submitPressed;
+        };
+
+        $scope.sendFeedback = function(form){
+            $scope.submitPressed = true;
+            if(form.$valid){
+                postFeedback();
             }
-        });
+        };
+
+        $scope.captchaImgSrcTime = new Date().getTime();
+
+        $scope.refreshCaptchaImg = function(){
+            $scope.captchaImgSrcTime = new Date().getTime();
+        };
     })
 
     .controller('FeedbackSendSuccessCtrl', function($scope){
